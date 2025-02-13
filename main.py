8d45b1f2668a6dd8ce5803151b18a345e97dd381
BaoBea <natphumin.th@gmail.com>
2025-02-13 17:15:37 +0000

    Major changes

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from flask import Flask, session, render_template, redirect, request, send_from_directory
 from json import JSONEncoder, JSONDecoder, JSONDecodeError
 app = Flask(__name__)
-app.template_folder = 'websiteGGG'
+app.template_folder = 'temps'
 
 @app.route('/')
 def homepage():
     return render_template('HomePage.html')
 
+@app.route('/app')
+def apphome():
+    return redirect('app/eval')
+
+@app.route('/app/eval')
+def appeval():
+    return render_template('eval.html')
+
 @app.route('/files/<path>')
 def downloadfile(path):
     print(path)
-    return send_from_directory('static', path)
+    return send_from_directory('files', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
