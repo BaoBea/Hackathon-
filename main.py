eb0f1ffb288157a3278c99fdeb25144c2ef6c1de
BaoBea <natphumin.th@gmail.com>
2025-02-13 08:07:50 +0000

    major changes

--- main.py
+++ main.py
@@ -5,4 +5,19 @@ app.template_folder = 'websiteGGG'
 
 @app.route('/')
 def homepage():
-    return render_template('homepage.html')
+    return render_template('HomePage.html')
+
+@app.route('/image/<path>')
+def imagepath(path):
+    print(path)
+    # Open the file in read mode
+    file = open('websiteGGG/'+path, "r")
+    # Read the entire content of the file
+    content = file.read()
+    # Print the content
+    # Close the file
+    file.close()
+    return file
+
+if __name__ == '__main__':
+    app.run(debug=True)
